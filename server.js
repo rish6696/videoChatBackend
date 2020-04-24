1da382e06a679b57643765c588983c7587267c47
rishabh <rish6696.rs@gmail.com>
2020-04-24 22:30:28 +0530

    deploying on heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.port || 5896;
+const PORT = process.env.PORT || 5896;
 const http = require('http');
 const socket = require('socket.io');
 
